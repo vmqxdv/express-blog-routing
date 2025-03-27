0712b1ed7316045220ab98076b7e7afb00150a94
vmqxdv <vquintilio@gmail.com>
2025-03-27 17:27:35 +0100

    fix: Corretta la riposta in modo da non generare più errore

--- routes/routers.js
+++ routes/routers.js
@@ -8,19 +8,19 @@ router.get('/', (_, res) => {
 });
 
 router.post('/', function (req, res) {
-  res.send('Aggiunto nuovo dolce: ', req);
+  res.send('Aggiunto nuovo dolce: ' + req.params.id);
 });
 
 router.put('/:id', function (req, res) {
-  res.send('[PUT] Modificato dolce: ', req.params.id);
+  res.send('[PUT] Modificato dolce: ' + req.params.id);
 });
 
 router.patch('/:id', function (req, res) {
-  res.send('[PATCH] Modificato il dolce: ', req.params.id);
+  res.send('[PATCH] Modificato il dolce: ' + req.params.id);
 });
 
 router.delete('/:id', function (req, res) {
-  res.send('Eliminato il dolce: ', req.params.id);
+  res.send('Eliminato il dolce: ' + req.params.id);
 });
 
 
